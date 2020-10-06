b04355f6b30825692b080b3932ee69081faf9ddb
Abd. Asis <id.adbasis@gmail.com>
2020-10-06 10:32:57 +0700

    update data menu

--- resources/views/frontend/includes/navbar.blade.php
+++ resources/views/frontend/includes/navbar.blade.php
@@ -24,9 +24,15 @@
                 <li class="nav-item">
                     <a href="{{ route('produk.index') }}" class="nav-link">Produk</a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{ route('berita') }}" class="nav-link">Service</a>
-                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Service
+                      <span><i class="mdi mdi-arrow-down"></i></span>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('trading') }}">Trading</a>
+                      <a class="dropdown-item" href="{{ route('produk.index') }}">Product</a>
+                  </li>
                 <li class="nav-item">
                     <a href="{{ route('contact-us.index') }}" class="nav-link">Hubungi Kami</a>
                 </li>

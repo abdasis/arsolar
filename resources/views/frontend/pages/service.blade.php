8552c884471ad210b9521ad61b150ff1e79164ab
Abd. Asis <id.adbasis@gmail.com>
2020-10-08 13:02:38 +0700

    update data sevice

--- resources/views/frontend/pages/service.blade.php
+++ resources/views/frontend/pages/service.blade.php
@@ -4,74 +4,33 @@
     <div class="container mt-100">
         <div class="row">
             <div class="col-md-12 mb-4">
-                <h3 class="text-center">
-                    Profile
-                </h3>
-                <p class="text-center font-16 ">
-                    PT. Anugrah Raya Energy solusi energi terbarukan dengan bernagai jenis produk yang kami tawarkan. Dengan produk yang berkuliatas dan layanan service kami dengan berbagai jenis.                </p>
+                <div class="card shadow-sm py-5 px-3 border-top">
+                    <h3 class="text-center">
+                        Profile
+                    </h3>
+                    <div class="row">
+                        <div class="col-md-4"><img class="float-right" src="https://solar.ar-solarwindenergy.com/wp-content/uploads/2020/09/are-300x158.png" height="80" alt=""></div>
+                        <div class="col-md-8">
+                            PT. Anugrah Raya Energy solusi energi terbarukan dengan bernagai jenis produk yang kami tawarkan. Dengan produk yang berkuliatas dan layanan service kami dengan berbagai jenis.   
+                            
+                            <ol type="1" class="fa-list-ol p-0 mt-2 ml-2">
+                                <li>Test Tegangan Tembus</li>
+                                <li>Treatment oli trafo</li>
+                                <li>Repaire transformer</li>
+                                <li>Analisa gas terlarut pada oli dan test kondisi fisik oli trafo</li>
+                                <li>Service cubical (MVMDB), Capasitor Bank, dan Panel (LVMDB)</li>
+                                <li>Pengadaan oli trafo baru</li>
+                                <li>Pengadaan trafo, reinstal trafo dan kabel 20 Kv</li>
+                                <li>Perbaikan, perawatan (Maintenance) genset dan alternator</li>
+                                <li>Installation building mechanical dan electrical.</li>
+                            </ol>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Test Tegangan Tembus</h4>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Treatment oli trafo</h4>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Repaire transformer </h4>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Analisa gas terlarut pada oli dan test kondisi fisik oli trafo</h4>
-                    </div>
-                </div>
-            </div><div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Service cubical (MVMDB), Capasitor Bank, dan Panel (LVMDB)</h4>
-                    </div>
-                </div>
-            </div><div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Pengadaan trafo, reinstal trafo dan kabel 20 Kv </h4>
-                    </div>
-                </div>
-            </div><div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Perbaikan, perawatan (Maintenance) genset dan alternator</h4>
-                    </div>
-                </div>
-            </div><div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Installation building mechanical dan electrical.</h4>
-                    </div>
-                </div>
-            </div>
-             <div class="col-md-4">
-                <div class="card shadow-md border-bottom">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Pengadaan oli trafo baru </h4>
-                    </div>
-                </div>
-            </div>
-        </div>
+     
     </div>
 @endsection

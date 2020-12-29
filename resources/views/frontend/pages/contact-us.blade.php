d0ddfa3ed48cbd8273538f1504816c20c671b097
Abd. Asis <id.abdasis@gmail.com>
2020-12-28 21:09:02 -0800

    Update contact-us.blade.php

--- resources/views/frontend/pages/contact-us.blade.php
+++ resources/views/frontend/pages/contact-us.blade.php
@@ -3,73 +3,79 @@
 
 @section('content')
 
-    <div class="container-fluid">
-        <div class="row section-breadcrumb">
-            <nav aria-label="breadcrumb ">
-                <ol class="breadcrumb pl-2">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Library</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Data</li>
-                </ol>
-            </nav>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-10 my-5">
-                <div class="row mb-2">
-                    <div class="col-md-4">
-                        <div class="card p-1">
-                            <div class="row align-items-center">
-                                <div class="col-md-3"><i class="mdi mdi-map-marker  mx-auto mt-4 mdi-36px"></i></div>
-                                <div class="col-md-9">Ruko The Palem Residence NO. 38B Jl. H. Nausan—Sriamur Tambun Utara ( Samping Kantor Camat Tambun Utara ) <br> West Java <br>   Indonesia</div>
-                            </div>
+<div class="container-fluid">
+    <div class="row section-breadcrumb">
+        <nav aria-label="breadcrumb ">
+            <ol class="breadcrumb pl-2">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Library</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Data</li>
+            </ol>
+        </nav>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-10 my-5">
+            <div class="row mb-2">
+                <div class="col-md-4">
+                    <div class="card p-1">
+                        <div class="row align-items-center">
+                            <div class="col-md-3"><i class="mdi mdi-map-marker  mx-auto mt-4 mdi-36px"></i></div>
+                            <div class="col-md-9">Ruko The Palem Residence NO. 38B <br>
+                                Jl. H. Nausan Sriamur Tambun Utara <br>
+                                (Samping Kantor Camat Tambun Utara ) <br>
+                                West Java, Indonesia</div>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="card p-1">
-                            <div class="row align-items-center">
-                                <div class="col-md-3"><i class="mdi mdi-phone mx-auto mt-4 mdi-36px"></i></div>
-                                <div class="col-md-9"><span class="pt-3">+62811-152-528 <br> +62812-9591-7075</span></div>
-                            </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-1">
+                        <div class="row align-items-center">
+                            <div class="col-md-3"><i class="mdi mdi-phone mx-auto mt-4 mdi-36px"></i></div>
+                            <div class="col-md-9"><span class="pt-3">+62811-152-528 <br> +62812-9591-7075</span></div>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="card p-1">
-                            <div class="row align-items-center">
-                                <div class="col-md-3"><i class="mdi mdi-gmail mx-auto mt-4 mdi-36px"></i></div>
-                                <div class="col-md-9">
-                                    <span>agussalim@ar-solarwindenergy.com <br> sabar@ar-solarwindenergy.com</span>
-                                </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-1">
+                        <div class="row align-items-center">
+                            <div class="col-md-3"><i class="mdi mdi-gmail mx-auto mt-4 mdi-36px"></i></div>
+                            <div class="col-md-9">
+                                <span>agussalim@ar-solarwindenergy.com <br> sabar@ar-solarwindenergy.com</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="contact-wrapper shadow p-3 rounded">
-                    <form action="" method="post">
-                        @csrf
-                        <div class="form-group row">
-                            <div class="col-md-6">
-                                <label for="nama">{{ GoogleTranslate::trans('Nama Lengkap', Session::get('bahasa') ?? 'id') }}</label>
-                                <input type="text" class="form-control" placeholder="{{ GoogleTranslate::trans('Masukan Nama Lengkap', Session::get('bahasa') ?? 'id') }}">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="nama">Alamat Email</label>
-                                <input type="text" class="form-control" placeholder="Masukan Alamat Email">
-                            </div>
+            </div>
+            <div class="contact-wrapper shadow p-3 rounded">
+                <form action="" method="post">
+                    @csrf
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label
+                                for="nama">{{ GoogleTranslate::trans('Nama Lengkap', Session::get('bahasa') ?? 'id') }}</label>
+                            <input type="text" class="form-control"
+                                placeholder="{{ GoogleTranslate::trans('Masukan Nama Lengkap', Session::get('bahasa') ?? 'id') }}">
                         </div>
-                        <div class="form-group">
-                            <label for="subject">Subject</label>
-                            <input type="text" class="form-control" placeholder="Masukan Subject Pesan">
+                        <div class="col-md-6">
+                            <label for="nama">Alamat Email</label>
+                            <input type="text" class="form-control" placeholder="Masukan Alamat Email">
                         </div>
-                        <div class="form-group">
-                            <label for="content">Isi Pesan</label>
-                            <textarea name="content" id="content" cols="30" rows="10" class="form-control" placeholder="Masukan Isi Pesan"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-success">Kirim Pesan</button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="subject">Subject</label>
+                        <input type="text" class="form-control" placeholder="Masukan Subject Pesan">
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Isi Pesan</label>
+                        <textarea name="content" id="content" cols="30" rows="10" class="form-control"
+                            placeholder="Masukan Isi Pesan"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success">Kirim Pesan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endsection

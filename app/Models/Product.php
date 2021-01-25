b160fe3f5897831ddf5562b6bdbea2297acd8048
Abd. Asis <id.abdasis@gmail.com>
2021-01-25 15:51:13 +0700

    tambah data translate

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Product extends Model
 {
+    use HasTranslations;
+
+    public $translatable = ['name'];
     public function categories()
     {
         return $this->hasMany(Category::class);
